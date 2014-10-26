3bfba4beaef1e9b08c735a6e5a0c5be24ad3f9da
LEADR <ian.fennie@gmail.com>
2014-10-25 20:34:39 -0700

    Comments added to app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+// Gets time
 function getNow() {
+  // Formats tim
   var result = moment().format("YYYY-MM-DD | HH:mm:ss:SS");
   return result;
 }
 
+// Displays time in DOM, and refreshes every half second
 function localClock() {
   $(".local-time").text(getNow());
   t = setTimeout(function() {
@@ -10,14 +13,17 @@ function localClock() {
   }, 500);
 }
 
+// makes AJAX call to the server
 function asyncDemo(url) {
   $.ajax({
     type: "GET",
     url: url,
+    // Timestamps AJAX call in DOM
     beforeSend : function() {
       $(".results").append("<p>Request sent at: " + getNow() + "</p>");
     }
   })
+  // Timestamps response once it is returned
   .done(function(data) {
     $(".results")
       .append("<p>Response at: " + getNow() + "</p>")
@@ -28,8 +34,10 @@ function asyncDemo(url) {
   });
 }
 
+// Defined globally to be accessed within doc ready block and AJAX call
 var url = "http://echo.jsontest.com/hello/world";
 
+// Adds updating clock and makes AJAX call to server
 $(document).ready(function(){
   localClock();
   asyncDemo(url);
